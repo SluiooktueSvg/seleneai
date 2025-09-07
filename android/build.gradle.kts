e902bb113273c1b817fed3500fb7638118ad9245
canchilamario100 <canchilamario100@gmail.com>
2025-09-07 04:10:13 +0000

    32 commit

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,11 +1,24 @@
-
-plugins {
-    id("com.android.application") version "7.3.0" apply false
-    id("com.google.gms.google-services") version "4.3.15" apply false
-    id("org.jetbrains.kotlin.android") version "1.7.10" apply false
-    id("dev.flutter.flutter-gradle-plugin") version "1.0.0" apply false
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
+    }
 }
 
-tasks.register("clean", Delete::class) {
-    delete(rootProject.buildDir)
+val newBuildDir: Directory =
+    rootProject.layout.buildDirectory
+        .dir("../../build")
+        .get()
+rootProject.layout.buildDirectory.value(newBuildDir)
+
+subprojects {
+    val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
+    project.layout.buildDirectory.value(newSubprojectBuildDir)
+}
+subprojects {
+    project.evaluationDependsOn(":app")
+}
+
+tasks.register<Delete>("clean") {
+    delete(rootProject.layout.buildDirectory)
 }
