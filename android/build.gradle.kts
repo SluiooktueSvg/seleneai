7bcadacb6740bad8e9f72469e041bbf7372b3c68
canchilamario100 <canchilamario100@gmail.com>
2025-09-07 04:44:33 +0000

    37 commit

--- android/build.gradle.kts
+++
@@ -1,9 +0,0 @@
-subprojects {
-    project.afterEvaluate {
-        if(project.name == "speech_to_text") {
-            android {
-                namespace = "com.csdcorp.speech_to_text"
-            }
-        }
-    }
-}
